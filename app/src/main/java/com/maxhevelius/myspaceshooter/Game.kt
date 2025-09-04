764c754db47bdcbde71e68f834e6d11004f2e2eb
Max-the-grand-wizard <max.hevelius@gmail.com>
2025-09-04 18:48:07 +0200

    Added Entity and Star

--- app/src/main/java/com/maxhevelius/myspaceshooter/Game.kt
+++ app/src/main/java/com/maxhevelius/myspaceshooter/Game.kt
@@ -1,14 +1,26 @@
 package com.maxhevelius.myspaceshooter
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.Log
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 
-class Game(context: Context?) : SurfaceView(context), Runnable {
+class Game(context: Context?) : SurfaceView(context), Runnable, SurfaceHolder.Callback {
     private val TAG = "Game"
     lateinit var gameThread : Thread
     @Volatile var isRunning : Boolean = false
+    val star = Star()
+
+
+    init {
+        holder?.addCallback(this)
+    }
+
+
     override fun run() {
+        Log.d(TAG, "run()")
         while (isRunning){
             update()
             render()
@@ -16,29 +28,46 @@ class Game(context: Context?) : SurfaceView(context), Runnable {
     }
 
     private fun render() {
-        //lock and acquire a surface to draw to
-        //draw all entities
-        //unlock the surface, and post it for the system for drawing
+        val canvas = holder?.lockCanvas() ?: return
+        canvas.drawColor(Color.BLUE)
+        val paint= Paint()
+
+        //render all entities
+        star.render(canvas, paint )
+        holder.unlockCanvasAndPost(canvas)
+
     }
 
     private fun update() {
         //update all objects(entities)
-        Log.d(TAG, "Update()")
+        star.update()
+
     }
 
     fun onPause(){
-        isRunning = false
-        gameThread.join()
         Log.d(TAG, "onPause()")
-
     }
 
     fun onResume(){
         Log.d(TAG, "onResume()")
+
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        Log.d(TAG, "surfaceCreated()")
         isRunning = true
         gameThread =Thread(this)
         gameThread.start()
+    }
 
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        Log.d(TAG, "surfaceChanged()")
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        Log.d(TAG, "surfaceDestroyed()")
+        isRunning = false
+        gameThread.join()
     }
 
 
